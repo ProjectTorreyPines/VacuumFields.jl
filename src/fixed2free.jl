2fb6b512f22d73c8c0b2e4f092a7b72e84872e08
Orso Meneghini <orso82@gmail.com>
2024-12-20 12:45:55 -0800

    better optimal_λ_regularize

--- src/fixed2free.jl
+++ src/fixed2free.jl
@@ -49,8 +49,8 @@ function optimal_λ_regularize(
     λ_range_exp = min_exp:0.5:max_exp
     cost_λ = λ -> cost_λ_regularize(λ, coils, EQ, image; flux_cps, saddle_cps, iso_cps, ψbound, fixed_coils, Sb)
     costs = [log10(cost_λ(λ)) for λ in λ_range_exp]
-    costs = costs .- minimum(costs)
-    costs = costs .+ λ_range_exp ./ max_exp .* maximum(costs) * 0.1
+    costs = (costs .- minimum(costs)) ./ (maximum(costs) - minimum(costs))
+    costs = costs .+ range(1.0, 0.0, length(λ_range_exp)) .^ 4
     opti_λ = λ_range_exp[argmin(costs)]
     return 10^opti_λ
 end
