7222dde2b682f040e9fe0421e6b1ad8df3daec9c
Orso Meneghini <orso82@gmail.com>
2025-09-29 22:28:29 -0700

    more robust magnetic_control_points()

--- src/imas_control.jl
+++ src/imas_control.jl
@@ -114,71 +114,76 @@ function magnetic_control_points(
     magnetic_probe_weight::T=1.0,
     magnetic_probe_weights::AbstractVector{T}=T[]) where {T<:Real}
 
-    # Probe control points (Note: type and size ignored)
-    min_σ = minimum(IMAS.@ddtime(probe.field.data_σ) for probe in mags.b_field_pol_probe)
     field_cps = FieldControlPoint{T}[]
-    for (k, probe) in enumerate(mags.b_field_pol_probe)
-        if (!isempty(probe.field.validity) && probe.field.validity < 0) || !isempty(probe.field.data_σ) && IMAS.@ddtime(probe.field.data_σ) == 0.0
-            continue
-        end
-        weight = (isempty(magnetic_probe_weights) ? 1.0 : magnetic_probe_weights[k]) * magnetic_probe_weight / length(mags.b_field_pol_probe) * min_σ / IMAS.@ddtime(probe.field.data_σ)
-        if weight > 0.0
-            #IMAS allows probes to mix toroidal and poloidal fields so that needs to be accounted for
-            bpol_field = isempty(probe.toroidal_angle) ? IMAS.@ddtime(probe.field.data) : IMAS.@ddtime(probe.field.data) * (1 - cos(probe.poloidal_angle) * sin(probe.toroidal_angle))
-            push!(field_cps, FieldControlPoint{T}(probe.position.r, probe.position.z, probe.poloidal_angle, bpol_field, weight))
+    flux_cps = FluxControlPoint{T}[]
+    iso_cps = IsoControlPoint{T}[]
+
+    # Probe control points (Note: type and size ignored)
+    if !isempty(mags.b_field_pol_probe)
+        min_σ = minimum(IMAS.@ddtime(probe.field.data_σ) for probe in mags.b_field_pol_probe)
+        for (k, probe) in enumerate(mags.b_field_pol_probe)
+            if (!isempty(probe.field.validity) && probe.field.validity < 0) || !isempty(probe.field.data_σ) && IMAS.@ddtime(probe.field.data_σ) == 0.0
+                continue
+            end
+            weight = (isempty(magnetic_probe_weights) ? 1.0 : magnetic_probe_weights[k]) * magnetic_probe_weight / length(mags.b_field_pol_probe) * min_σ / IMAS.@ddtime(probe.field.data_σ)
+            if weight > 0.0
+                #IMAS allows probes to mix toroidal and poloidal fields so that needs to be accounted for
+                bpol_field = isempty(probe.toroidal_angle) ? IMAS.@ddtime(probe.field.data) : IMAS.@ddtime(probe.field.data) * (1 - cos(probe.poloidal_angle) * sin(probe.toroidal_angle))
+                push!(field_cps, FieldControlPoint{T}(probe.position.r, probe.position.z, probe.poloidal_angle, bpol_field, weight))
+            end
         end
     end
 
     # Flux loop control points (Note: type and size ignored)
-    iref = reference_flux_loop_index
-    loops = mags.flux_loop
-    @assert 0 <= iref <= length(loops)
-    flux_cps = FluxControlPoint{T}[]
-    iso_cps = IsoControlPoint{T}[]
-    if iref > 0
-        @assert isempty(loops[iref].flux.validity) || loops[iref].flux.validity >= 0
-        # Fit a single reference flux value and the difference between the other flux_loops and this value (this matches how data is collected on DIII-D and fit with EFIT)
+    if !isempty(mags.flux_loop)
+        iref = reference_flux_loop_index
+        loops = mags.flux_loop
+        @assert 0 <= iref <= length(loops)
+        if iref > 0
+            @assert isempty(loops[iref].flux.validity) || loops[iref].flux.validity >= 0
+            # Fit a single reference flux value and the difference between the other flux_loops and this value (this matches how data is collected on DIII-D and fit with EFIT)
 
-        # Convert from total flux uncertainty (IMAS convention) to differential
-        relative_σ(k) = sqrt(IMAS.@ddtime(loops[k].flux.data_σ)^2 + IMAS.@ddtime(loops[iref].flux.data_σ)^2)
-        min_σ = minimum(relative_σ(ck) for ck in 1:length(loops) if ck != iref)
+            # Convert from total flux uncertainty (IMAS convention) to differential
+            relative_σ(k) = sqrt(IMAS.@ddtime(loops[k].flux.data_σ)^2 + IMAS.@ddtime(loops[iref].flux.data_σ)^2)
+            min_σ = minimum(relative_σ(ck) for ck in 1:length(loops) if ck != iref)
 
-        for ck in 1:length(loops)
-            if (ck == iref) || (!isempty(loops[ck].flux.validity) && loops[ck].flux.validity < 0) || (!isempty(loops[ck].flux.data_σ) && IMAS.@ddtime(loops[ck].flux.data_σ) == 0.0)
-                continue
-            end
-            weight = (isempty(flux_loop_weights) ? 1.0 : flux_loop_weights[ck]) * flux_loop_weight / length(loops) * min_σ / relative_σ(ck)
+            for ck in 1:length(loops)
+                if (ck == iref) || (!isempty(loops[ck].flux.validity) && loops[ck].flux.validity < 0) || (!isempty(loops[ck].flux.data_σ) && IMAS.@ddtime(loops[ck].flux.data_σ) == 0.0)
+                    continue
+                end
+                weight = (isempty(flux_loop_weights) ? 1.0 : flux_loop_weights[ck]) * flux_loop_weight / length(loops) * min_σ / relative_σ(ck)
 
-            if weight > 0.0
-                push!(
-                    iso_cps,
-                    IsoControlPoint{T}(
-                        loops[ck].position[1].r,
-                        loops[ck].position[1].z,
-                        loops[iref].position[1].r,
-                        loops[iref].position[1].z,
-                        IMAS.@ddtime(loops[ck].flux.data) - IMAS.@ddtime(loops[iref].flux.data),
-                        weight
+                if weight > 0.0
+                    push!(
+                        iso_cps,
+                        IsoControlPoint{T}(
+                            loops[ck].position[1].r,
+                            loops[ck].position[1].z,
+                            loops[iref].position[1].r,
+                            loops[iref].position[1].z,
+                            IMAS.@ddtime(loops[ck].flux.data) - IMAS.@ddtime(loops[iref].flux.data),
+                            weight
+                        )
                     )
-                )
+                end
             end
-        end
 
-        weight = (isempty(flux_loop_weights) ? 1.0 : flux_loop_weights[iref]) * flux_loop_weight / length(loops)
-        if weight > 0.0
-            push!(flux_cps, FluxControlPoint{T}(loops[iref].position[1].r, loops[iref].position[1].z, IMAS.@ddtime(loops[iref].flux.data), weight))
-        end
-
-    else
-        # Fit each flux loop separately (this is how data is collected on NSTX and fit with EFIT)
-        min_σ = minimum(IMAS.@ddtime(loop.flux.data_σ) for loop in loops)
-        for (k, loop) in enumerate(loops)
-            if (!isempty(loop.flux.validity) && loop.flux.validity < 0) || (!isempty(loop.flux.data_σ) && IMAS.@ddtime(loop.flux.data_σ) == 0.0)
-                continue
-            end
-            weight = (isempty(flux_loop_weights) ? 1.0 : flux_loop_weights[k]) * flux_loop_weight / length(loops) * min_σ / IMAS.@ddtime(loop.flux.data_σ)
+            weight = (isempty(flux_loop_weights) ? 1.0 : flux_loop_weights[iref]) * flux_loop_weight / length(loops)
             if weight > 0.0
-                push!(flux_cps, FluxControlPoint{T}(loop.position[1].r, loop.position[1].z, IMAS.@ddtime(loop.flux.data), weight))
+                push!(flux_cps, FluxControlPoint{T}(loops[iref].position[1].r, loops[iref].position[1].z, IMAS.@ddtime(loops[iref].flux.data), weight))
+            end
+
+        else
+            # Fit each flux loop separately (this is how data is collected on NSTX and fit with EFIT)
+            min_σ = minimum(IMAS.@ddtime(loop.flux.data_σ) for loop in loops)
+            for (k, loop) in enumerate(loops)
+                if (!isempty(loop.flux.validity) && loop.flux.validity < 0) || (!isempty(loop.flux.data_σ) && IMAS.@ddtime(loop.flux.data_σ) == 0.0)
+                    continue
+                end
+                weight = (isempty(flux_loop_weights) ? 1.0 : flux_loop_weights[k]) * flux_loop_weight / length(loops) * min_σ / IMAS.@ddtime(loop.flux.data_σ)
+                if weight > 0.0
+                    push!(flux_cps, FluxControlPoint{T}(loop.position[1].r, loop.position[1].z, IMAS.@ddtime(loop.flux.data), weight))
+                end
             end
         end
     end
